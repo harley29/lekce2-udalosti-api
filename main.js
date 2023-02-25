23146b190ea762d0522ac8eb3c395ade518adf01
Pavlina Behounkova <behounkovap@gmail.com>
2023-02-25 22:13:04 +0100

    úprava

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // tady je místo pro náš program
+
+let nadpis=document.querySelector("h1");
+nadpis.classList.add("csstrida")
+
+let ztucni=document.querySelector(".odstavec");
+
+function priNajeti(){
+    ztucni.classList.remove(fontWeight="bold")
+}
+
+let zmenBarvu=document.querySelector(".odstavec")
+zmenBarvu.classList.remove("red")
+
+let zvuk;
+zvuk=document.querySelector("#zvuk");
+  zvuk.play ();
+  zvuk.volume=0;
+  zvuk.volume=0.5;
+  zvuk.volume=1;
